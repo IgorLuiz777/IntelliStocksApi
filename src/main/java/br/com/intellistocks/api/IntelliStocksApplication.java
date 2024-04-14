c7dc3b2a4387503d2e53292b69806b752ad9d406
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-04-14 18:43:16 -0300

    building project

---
+++ src/main/java/br/com/intellistocks/api/IntelliStocksApplication.java
@@ -0,0 +1,13 @@
+package br.com.intellistocks.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliStocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/intellistocks/api/IntelliStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.intellistocks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
