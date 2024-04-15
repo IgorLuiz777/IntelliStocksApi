7d21033e2f52547e88c83bceb1ceaea8809b227e
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-04-15 08:56:31 -0300

    feat: adding get method - Produto

--- src/main/java/br/com/intellistocks/api/controller/ProdutoController.java
+++ src/main/java/br/com/intellistocks/api/controller/ProdutoController.java
@@ -2,9 +2,15 @@ package br.com.intellistocks.api.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
+
+import java.util.List;
+
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +30,28 @@ public class ProdutoController {
 
     @PostMapping()
     @ResponseStatus(CREATED)
-    public Produto cadastrarProduto(@RequestBody Produto produto) {
+    public Produto postProduto(@RequestBody Produto produto) {
 
         return produtoRepository.save(produto);
 
     }
+
+    @GetMapping()
+    public List<Produto> listProdutos() {
+
+        return produtoRepository.findAll();
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> getProdutoById(@PathVariable Long id) {
+
+        return produtoRepository
+        .findById(id)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+    }
+
 
     
 }
